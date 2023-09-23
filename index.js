a5d87cafc73feb5c8421f3ac5db032e68f28fda6
Anthony Dreessen <anthonydreessen@gmail.com>
2023-09-23 10:43:35 -0700

    author 1p version

--- index.js
+++ index.js
@@ -6,9 +6,18 @@ import { render } from 'resumed'
 const resume = JSON.parse(await fs.readFile('resume.json', 'utf-8'))
 const html = await render(resume, theme)
 
-const browser = await puppeteer.launch()
+const browser = await puppeteer.launch({headless: "new"})
 const page = await browser.newPage()
 
 await page.setContent(html, { waitUntil: 'networkidle0' })
-await page.pdf({ path: 'resume.pdf', format: 'a4', printBackground: true })
+await page.pdf({ path: 'resume.pdf', format: 'a4', printBackground: true, margin: {top: 5, bottom: 5}})
 await browser.close()
+
+const resume1p = JSON.parse(await fs.readFile('resume_1p.json', 'utf-8'))
+const html1p = await render(resume1p, theme)
+
+const browser1p = await puppeteer.launch({headless: "new"})
+const page1p = await browser1p.newPage()
+
+await page1p.setContent(html1p, { waitUntil: 'networkidle0' })
+await page1p.pdf({ path: 'resume_1p.pdf', format: 'a4', printBackground: true})
